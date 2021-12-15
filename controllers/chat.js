024af3673647229a9e14047a5b680d619ea004bd
liron <lirongolshmid11@gmail.com>
2021-12-15 14:53:32 +0200

    .

--- controllers/chat.js
+++ controllers/chat.js
@@ -40,7 +40,7 @@ export const sendMessage = async (req, res) => {
       });
 
       await existingChat.save();
-      const newMessage = existingChat.messages.at(-1);
+      const newMessage = existingChat.messages[-1];
       res.status(200).json({ newMessage, chatId: existingChat._id });
       
    } catch (err) {
